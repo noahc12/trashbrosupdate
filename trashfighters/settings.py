dad3ce85ddd4adc018888546a9c3a6cc4762f1f6
noahc12 <40617454+noahc12@users.noreply.github.com>
2018-10-27 15:21:45 -0700

    Add files via upload
    new update 10/27/18

---
+++ trashfighters/settings.py
@@ -0,0 +1,18 @@
+TITLE = "Trash Fighters!"
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+# Player properties
+PLAYER_ACC = 1
+PLAYER_FRICTION = -0.12
+PLAYER_GRAVITY = 0.8
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+LIGHTBLUE = (0, 155, 155)
